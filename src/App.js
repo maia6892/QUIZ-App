91a89840d9e399b60e58afda3690576189a2fab5
MAIA <nmaia6892@gmail.com>
2023-09-27 14:51:50 +0400

    second version

--- src/App.js
+++ src/App.js
@@ -1,40 +1,86 @@
 import React from "react";
-import Questions from "./components/Questions";
 import "./App.css";
 
 class App extends React.Component {
     questions = [
         {
-            id: 1,
-            question: "1 + 3",
-            answers: [3, 4, 5],
-            rightAnswer: 4,
+            questionText: "1 + 3 = ?",
+            answerOptions: [
+                { answerText: "4", isCorrect: true },
+                { answerText: "5", isCorrect: false },
+                { answerText: "7", isCorrect: false },
+                { answerText: "2", isCorrect: false },
+            ],
         },
         {
-            id: 2,
-            question: "2 + 4",
-            answers: [5, 6, 7],
-            rightAnswer: 6,
+            questionText: "5 + 3 = ?",
+            answerOptions: [
+                { answerText: "9", isCorrect: false },
+                { answerText: "8", isCorrect: true },
+                { answerText: "10", isCorrect: false },
+                { answerText: "7", isCorrect: false },
+            ],
         },
         {
-            id: 3,
-            question: "3 + 7",
-            answers: [9, 10, 11],
-            correctAnswer: 10,
+            questionText: "4 + 2 = ?",
+            answerOptions: [
+                { answerText: "4", isCorrect: false },
+                { answerText: "5", isCorrect: false },
+                { answerText: "6", isCorrect: true },
+                { answerText: "7", isCorrect: false },
+            ],
+        },
+        {
+            questionText: "8 + 2 = ?",
+            answerOptions: [
+                { answerText: "12", isCorrect: false },
+                { answerText: "11", isCorrect: false },
+                { answerText: "9", isCorrect: false },
+                { answerText: "10", isCorrect: true },
+            ],
         },
     ];
     constructor(props) {
         super(props);
         this.state = {
             currentQuestion: 0,
-            setCurrentQuestion: 0,
         };
-
     }
+    handleAnswerButtonClick = () => {
+        const nextQuestion = this.state.currentQuestion + 1;
+        if (nextQuestion < this.questions.length) {
+            this.setState({
+                currentQuestion: this.state.currentQuestion + 1,
+            });
+        } else {
+            alert("That's it!");
+        }
+    };
+
     render() {
         return (
-            <div>
-                <Questions questions = {this.state.questions} />
+            <div className="app">
+                <div className="question-section">
+                    <div className="qustions-count">
+                        <span>Question {this.state.currentQuestion + 1}</span>/
+                        {this.questions.length}
+                    </div>
+                    <div className="question-text">
+                        {
+                            this.questions[this.state.currentQuestion]
+                                .questionText
+                        }
+                    </div>
+                    <div className="answer-section">
+                        {this.questions[
+                            this.state.currentQuestion
+                        ].answerOptions.map((answerOption) => (
+                            <button onClick={this.handleAnswerButtonClick}>
+                                {answerOption.answerText}
+                            </button>
+                        ))}
+                    </div>
+                </div>
             </div>
         );
     }
